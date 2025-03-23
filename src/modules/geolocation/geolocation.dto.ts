7074d662c55d21a07d56fbe56d8d85a7bb25b705
iqmalakur <iakbarkurnia@gmail.com>
2025-03-23 07:00:50 +0700

    feat: create GeolocationService

---
+++ src/modules/geolocation/geolocation.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GeolocationResDto {
+  @ApiProperty({ example: 'Cimahi' })
+  public readonly name: string;
+
+  @ApiProperty({ example: 'Cimahi, Jawa Barat, Jawa, Indonesia' })
+  public readonly fullName: string;
+
+  @ApiProperty({ example: -6.8731527 })
+  public readonly latitude: number;
+
+  @ApiProperty({ example: 107.5423099 })
+  public readonly longitude: number;
+}
