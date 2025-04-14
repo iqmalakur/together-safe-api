49198bfcc22a498bc212f609cc81417199f05a1d
iqmalakur <iakbarkurnia@gmail.com>
2025-04-14 14:22:23 +0700

    fix: use token middleware for only /report endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { HttpMiddleware } from './middlewares/http.middleware';
 import { IncidentModule } from './modules/incident/incident.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { GeolocationModule } from './modules/geolocation/geolocation.module';
 import { ReportModule } from './modules/report/report.module';
 import { TokenMiddleware } from './middlewares/token.middleware';
-import { ReportController } from './modules/report/report.controller';
 
 @Module({
   imports: [AuthModule, IncidentModule, ReportModule, GeolocationModule],
@@ -13,6 +17,12 @@ import { ReportController } from './modules/report/report.controller';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(HttpMiddleware).forRoutes('*');
-    consumer.apply(TokenMiddleware).forRoutes(ReportController);
+
+    consumer
+      .apply(TokenMiddleware)
+      .forRoutes(
+        { path: 'report', method: RequestMethod.GET },
+        { path: 'report', method: RequestMethod.POST },
+      );
   }
 }
