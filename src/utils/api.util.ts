d401841ce706369f46a5190b32bebbb1e4205851
iqmalakur <iakbarkurnia@gmail.com>
2025-04-18 14:18:03 +0700

    feat: add User-Agent for request to nominatim

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -6,6 +6,11 @@ export const getLocationName = async (
 ): Promise<string> => {
   const response: AxiosResponse<{ display_name: string }> = await axios.get(
     `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`,
+    {
+      headers: {
+        'User-Agent': 'TogetherSafe/1.0 (iqmalak21@if.unjani.ac.id)',
+      },
+    },
   );
 
   return response.data.display_name;
