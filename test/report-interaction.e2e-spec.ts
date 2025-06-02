208c4afc4feed35fe815ad5e0f696a22083456af
iqmalakur <iakbarkurnia@gmail.com>
2025-06-02 10:42:30 +0700

    feat: initialize e2e test for report interaction

---
+++ test/report-interaction.e2e-spec.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/infrastructures/prisma.service';
+
+describe('ReportInteractionController (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.init();
+
+    prisma = moduleFixture.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
